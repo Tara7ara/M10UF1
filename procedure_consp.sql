750c07e768f4245d312014075fabce0fec919c36
Martí T <marti.tarrason@enti.cat>
2023-05-27 18:55:40 +0200

    DEFERES FINAL HECHO CREO

--- procedure_consp.sql
+++ procedure_consp.sql
@@ -1,19 +1,24 @@
 DELIMITER $
-DROP PROCEDURE IF EXISTS InsertarOpinion;
-CREATE PROCEDURE InsertarOpinion(
-    IN id_usuario INT,
-    IN id_conspiracion INT,
-    IN opinion BOOLEAN
+
+DROP PROCEDURE IF EXIST ConvencerUsuario;
+
+CREATE PROCEDURE ConvencerUsuario(
+        IN id_usuario INT,
+        IN id_conspiracion INT,
+        IN convincente BOOLEAN
 )
 BEGIN
-    START TRANSACTION;
-    
-    INSERT INTO user_consp (id_user, id_consp) VALUES (id_usuario, id_conspiracion);
 
-    IF opinion THEN
-        COMMIT;
-    ELSE
-        ROLLBACK;
-    END IF;
+        START TRANSACTION;
+
+	INSERT INTO user_consp (id_user, id_consp) VALUES (id_user, id_consp);
+
+	IF opinion THEN
+                COMMIT;
+        ELSE
+                ROLLBACK;
+        END IF;
+
 END $
+
 DELIMITER ;
--- script_consp.sql
+++ script_consp.sql
@@ -1,26 +1,24 @@
-DROP TABLE IF EXISTS user_consp;
-DROP TABLE IF EXISTS conspiraciones;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  	id_user INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT
+);
+
+DROP TABLE IF EXISTS users_cosp;
+CREATE TABLE users_cosp (
+  	id_user_cosp INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_user INT UNSIGNED
+);
+
+DROP TABLE IF EXISTS cosp;
+CREATE TABLE cosp (
+        id_cosp INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        id_user_cosp INT UNSIGNED
+);
+
 DROP TABLE IF EXISTS iluminatis;
-
-CREATE TABLE conspiraciones(
-
-id_consp INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(128),
-description TEXT
-);
-
-CREATE TABLE user_consp(
-id_user_consp INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-id_user INT UNSIGNED NOT NULL,
-id_consp INT UNSIGNED NOT NULL,
-FOREIGN KEY (id_user) REFERENCES users(id_user),
-FOREIGN KEY (id_consp) REFERENCES conspiraciones(id_consp)
-);
-
-CREATE TABLE iluminatis(
-id_iluminati INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-id_user INT UNSIGNED NOT NULL,
-FOREIGN KEY (id_user) REFERENCES users(id_user)
+CREATE TABLE cosp (
+        id_user_iluminatis INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        id_user INT UNSIGNED
 );
 
 --Lo que teniamos antes
